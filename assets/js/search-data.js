9bbbebab98fa249cd32d4414667edb25c357aeb1
richardmahuze <56782400+richardmahuze@users.noreply.github.com>
2026-05-20 04:16:29 +0000

    Deploying to gh-pages from @ richardmahuze/richardmahuze.github.io@c148ce76882548d98dd6c9e1332de04a23fdc278 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -43,6 +43,27 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/personal-website/";
             },},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%72%61%6D%35%35%32@%63%6F%72%6E%65%6C%6C.%65%64%75", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/richardmahuze", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=71c33jgAAAAJ", "_blank");
+        },
+      },{
         id: 'social-github',
         title: 'GitHub',
         section: 'Socials',
